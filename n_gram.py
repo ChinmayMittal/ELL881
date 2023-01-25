8dc9a2d62d160efca628c65906c9cfcc4a0c6cee
Chinmay Mittal <chinmaymittal@Chinmays-MacBook-Air-2.local>
2023-01-25 16:16:51 +0530

    add-k smoothing

--- n_gram.py
+++ n_gram.py
@@ -37,16 +37,33 @@ class NGramLM():
             else:
                 self.n_gram_counter[ngram] = 1
                 
-                
-    def probability_for_next_word(self, context, token, add_1_smoothing=True):
+    def add_k_smoothing(self, k=1):
+        
+        all_context = self.context.keys()
+        for context in all_context:
+            for word in self.vocabulary:
+                for _ in range(k):
+                    self.context[context].append(word)
+                    if (context, word) in self.n_gram_counter:
+                        self.n_gram_counter[(context,word)] += 1
+                    else:
+                        self.n_gram_counter[(context,word)] = 1
+        
+                  
+    def probability_for_next_word(self, context, token):
         ### probability of token given context
+        if token not in self.vocabulary:
+            token = "<unk>"
+        if context not in self.context:
+            return 0.0
+        
         try:
-            n_gram_count = self.n_gram_counter[(context, token)]
-            context_count = float(len(self.context[context]))
-            if add_1_smoothing:
-                prob = (n_gram_count+1)/(context_count + len(self.vocabulary))
+            if ((context, token) not in self.n_gram_counter):
+                n_gram_count = 0
             else:
-                prob = n_gram_count / context_count
+                n_gram_count =  self.n_gram_counter[(context, token)]
+            context_count = float(len(self.context[context]))
+            prob = n_gram_count / context_count
         except:
             prob = 0.0
         
@@ -95,8 +112,8 @@ class NGramLM():
         
  
 train_books = range(1,7)
-LM = NGramLM(n=3)
-### TRAINING
+LM = NGramLM(n=1)
+########### TRAINING ###################
 for book in train_books:
     train_book = f"./Harry_Potter_Text/Book{book}.txt"
     print(f"{train_book} ....")
@@ -105,7 +122,10 @@ for book in train_books:
     for sent in tokenized_sentences:
         LM.update(sent)
 
-### TESTING 
+#### add-k smoothing ###################
+LM.add_k_smoothing(k=1)
+
+################### TESTING ############ 
 test_book = f"./Harry_Potter_Text/Book7.txt"    
 test_text = read_data(test_book)
 test_tokenized_sentences = preprocess(test_text)
@@ -118,4 +138,4 @@ for sent in test_tokenized_sentences:
     else:
         inf_count += 1
 print(f"{inf_count/len(test_tokenized_sentences)*100}%")
-# print( LM.generate_sentence(10) )
+print( LM.generate_sentence(10) )
